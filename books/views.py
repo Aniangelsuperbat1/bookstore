767f00dc8293940ede63b0c5eb5cf3fb1b63d9e1
Patrick <zhen.patrick@gmail.com>
2021-08-31 18:36:33 -0400

    loaded json data into views

--- books/views.py
+++ books/views.py
@@ -1,12 +1,13 @@
 from django.shortcuts import render
+import json
 
 # Create your views here.
 
+booksData = open('\Users\ZhenP\OneDrive\Desktop\django_bookstore\books.json')
 
+data = json.loads(booksData)
 
 def index(request):
-    context = {'book' : {
-        'title': 'new book',
-    }}
+    context = {'data': data}
     return render(request, 'books/index.html', context)
 
