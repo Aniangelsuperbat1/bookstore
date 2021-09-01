55d25c451e3be096ec766510e966bec5ebdf7db2
Patrick <zhen.patrick@gmail.com>
2021-09-01 18:31:23 -0400

    added to book model

--- books/models.py
+++ books/models.py
@@ -4,3 +4,8 @@ from django.db import models
 
 class Book(models.Model):
     title = models.CharField(max_length=200)
+    pageCount = models.IntegerField(default=0)
+    thumbnailUrl = models.CharField(max_length=200, null=True)
+    shortDescription = models.CharField(max_length=200)
+    longDescription = models.TextField(null=True)
+
