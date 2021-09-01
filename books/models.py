7bc4ee1482078601e3d4363a1ef4b1c700199a9f
Patrick <zhen.patrick@gmail.com>
2021-09-01 18:34:34 -0400

    finished book model

--- books/models.py
+++ books/models.py
@@ -6,6 +6,6 @@ class Book(models.Model):
     title = models.CharField(max_length=200)
     pageCount = models.IntegerField(default=0)
     thumbnailUrl = models.CharField(max_length=200, null=True)
-    shortDescription = models.CharField(max_length=200)
+    shortDescription = models.CharField(max_length=200, null=True)
     longDescription = models.TextField(null=True)
 
