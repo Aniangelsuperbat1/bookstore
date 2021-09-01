15164e4ca1d4b3b18a157df4e97cff364f9ad1a2
Patrick <zhen.patrick@gmail.com>
2021-09-01 18:56:37 -0400

    can query a single book from Book model

--- books/views.py
+++ books/views.py
@@ -9,11 +9,7 @@ def index(request):
     return render(request, 'books/index.html', context)
 
 def show(request, id):
-    singleBook = list()
-    for book in data:
-        if book['id'] == id:
-            singleBook = book
-
+    singleBook = Book.objects.get(pk=id)
     context = {'book': singleBook}
     return render(request, 'books/show.html', context)
 
