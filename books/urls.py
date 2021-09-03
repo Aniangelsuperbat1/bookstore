2c63c3c21a277764f8bc192df657ab0269009b3c
Patrick <zhen.patrick@gmail.com>
2021-09-03 10:18:34 -0400

    added review path

--- books/urls.py
+++ books/urls.py
@@ -5,5 +5,6 @@ from django.urls import path
 urlpatterns = [
     path("", views.new),
     path("book/", views.index, name ="book.all"),
-    path('<int:id>', views.show, name= "book.show")
+    path('<int:id>', views.show, name= "book.show"),
+    path('review', views.review, name="book.review")
 ]
