ae0ee38b02edd3fbc1c237b66ca1c462e0f7f9e7
Patrick <zhen.patrick@gmail.com>
2021-09-09 20:18:17 -0400

    authors names now show up in admin page

--- books/models.py
+++ books/models.py
@@ -3,6 +3,9 @@ from django.db import models
 # Create your models here.
 class Author(models.Model):
     name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
 class Book(models.Model):
     title = models.CharField(max_length=200)
     pageCount = models.IntegerField(default=0)
