f94cad1bc619910dfe7826dece07877e7ca1bb8a
Patrick <zhen.patrick@gmail.com>
2021-09-09 20:13:02 -0400

    changed authors to author for model

--- books/models.py
+++ books/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 # Create your models here.
-class Authors(models.Model):
+class Author(models.Model):
     name = models.CharField(max_length=255)
 class Book(models.Model):
     title = models.CharField(max_length=200)
@@ -9,7 +9,7 @@ class Book(models.Model):
     thumbnailUrl = models.CharField(max_length=200, null=True)
     shortDescription = models.CharField(max_length=200, null=True)
     longDescription = models.TextField(null=True)
-    authors = models.ManyToManyField(Authors)
+    authors = models.ManyToManyField(Author)
 
     def __str__(self):
         return f'{self.id} {self.title}'
