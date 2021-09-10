a97f8dff06be205d504171805ad16a70102df12d
Patrick <zhen.patrick@gmail.com>
2021-09-09 20:07:29 -0400

    migrated author model

--- books/models.py
+++ books/models.py
@@ -1,7 +1,8 @@
 from django.db import models
 
 # Create your models here.
-
+class Authors(models.Model):
+    name = models.CharField(max_length=255)
 class Book(models.Model):
     title = models.CharField(max_length=200)
     pageCount = models.IntegerField(default=0)
@@ -18,5 +19,4 @@ class Review(models.Model):
     created_at = models.DateTimeField(auto_now=True)
     book = models.ForeignKey(Book, on_delete=models.CASCADE, null=True)
 
-class Authors(models.Model):
-    name = models.CharField(max_length=255, null=True)
+
