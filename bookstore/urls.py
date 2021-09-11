9d8de5870d0aafaf074270647e06ccd77ff8fb4c
Patrick <zhen.patrick@gmail.com>
2021-09-10 20:16:59 -0400

    redirect to /book if already logged in

--- bookstore/urls.py
+++ bookstore/urls.py
@@ -15,10 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("", include('books.urls')),
     path("book/", include('books.urls')),
+    path('login/', auth_views.LoginView.as_view(redirect_authenticated_user=True)),
     path('', include('django.contrib.auth.urls')),
 ]
