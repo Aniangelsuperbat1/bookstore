9e4b900f4a9e937e800440496e7080c9b110561a
Patrick <zhen.patrick@gmail.com>
2021-09-16 20:33:05 -0400

    added null to image field in review model

--- books/models.py
+++ books/models.py
@@ -23,6 +23,6 @@ class Review(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
     created_at = models.DateTimeField(auto_now=True)
     book = models.ForeignKey(Book, on_delete=models.CASCADE, null=True)
-    image = models.ImageField(upload_to="images/review")
+    image = models.ImageField(upload_to="images/review", null=True)
 
 
