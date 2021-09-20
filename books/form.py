f055093d8edc127c05fe29c728d3e61c7ec64b3e
Patrick <zhen.patrick@gmail.com>
2021-09-20 19:49:35 -0400

    imaged not required on review

--- books/form.py
+++ books/form.py
@@ -2,4 +2,4 @@ from django import forms
 
 class ReviewForm(forms.Form):
     body = forms.CharField(widget=forms.Textarea(attrs={'class': 'border rounded p-2 w-full', 'placeholder': "Review"}))
-    image = forms.ImageField()
+    image = forms.ImageField(required=False)
