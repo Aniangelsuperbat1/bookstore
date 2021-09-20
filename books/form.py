0e6b9f169ae1f115f1fc078cf2f3a26175ccf0cb
Patrick <zhen.patrick@gmail.com>
2021-09-20 19:49:07 -0400

    added some styling to form

--- books/form.py
+++ books/form.py
@@ -1,5 +1,5 @@
 from django import forms
 
 class ReviewForm(forms.Form):
-    body = forms.CharField(widget=forms.Textarea)
+    body = forms.CharField(widget=forms.Textarea(attrs={'class': 'border rounded p-2 w-full', 'placeholder': "Review"}))
     image = forms.ImageField()
